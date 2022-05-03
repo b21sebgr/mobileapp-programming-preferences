bec7ee7f92c180a5e7b64b45a9a98374bfa7be54
b21sebgr <102599253+b21sebgr@users.noreply.github.com>
2022-05-03 11:26:42 +0200

    Added preferences in MainActivity

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.project;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private SharedPreferences myPreferenceRef;
+    private Button changeActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +22,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        myPreferenceRef = getPreferences(MODE_PRIVATE);
+        textView = findViewById(R.id.shared_pref_textview);
+        changeActivity = findViewById(R.id.second_activity_button);
+        changeActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
+    @Override
+    protected void onResume() {
+        textView.setText(myPreferenceRef.getString("TextPref", "No preference found"));
+    }
 }
