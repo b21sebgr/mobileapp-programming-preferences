a100d3a9d5e6be87c887dbcebe8893cc044b6c13
b21sebgr <102599253+b21sebgr@users.noreply.github.com>
2022-05-03 11:26:59 +0200

    Added preferences in SecondActivity

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -2,13 +2,33 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private SharedPreferences.Editor myPreferenceEditor;
+    private EditText input;
+    private Button setPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        myPreferenceEditor = getPreferences(MODE_PRIVATE).edit();
+
+        input = findViewById(R.id.input_view);
+        setPref = findViewById(R.id.set_pref);
+        setPref.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myPreferenceEditor.putString("MyAppPreferenceString", setPref.getText().toString());
+                myPreferenceEditor.apply();
+            }
+        });
     }
 }
